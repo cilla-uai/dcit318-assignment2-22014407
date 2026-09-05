b3885c1d4bf66b1ddd3606297abad137abe9bac2
Miss Cilla <206965870+cilla-uai@users.noreply.github.com>
2026-09-05 07:17:05 +0700

    Add inheritance method overriding assignment

---
+++ InheritanceMethodOverriding/Inheritance Method Overriding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Base class
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        Console.WriteLine("Animal sounds:");
+        genericAnimal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+
+        Console.ReadKey();
+    }
+}
